49177b77c12eef441ad17e6d06ba81d27d7bb605
Francesco Lobbia <142682839+FraLobbia@users.noreply.github.com>
2024-02-12 14:31:26 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <main>
+       
+       
+        <asp:Button ID="Button1" runat="server" Text="Clicca per mostrare il tuo nome" OnClick="Button1_Click"/>
+        <p id="testParagrafo" runat="server"></p>
+        
+
+    </main>
+
+</asp:Content>
---
+++ WebApplication1/Default.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            
+            
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = ConfigurationManager.AppSettings["Name"];
+            string surname = ConfigurationManager.AppSettings["surname"];
+            testParagrafo.InnerText = "Il mio nome è "+ surname + " " + name;
+        }
+    }
+}
